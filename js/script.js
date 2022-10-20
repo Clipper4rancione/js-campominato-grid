111d4ff9476f713094e3cff1e26d9c2445d0e519
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-20 16:53:14 +0200

    [add] dynamic squares on DOM with js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/**
+ * L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+ */
+
+//SELEZIONO IL CONTAINER
+const grid = document.getElementById('grid');
+//QUANTI BOX CI SONO PER ROW
+const boxPerRow = 10;
+
+init(boxPerRow);
+
+function init(nBox){
+    const totalBox = Math.pow(nBox, 2);
+
+    for(let i = 0; i < totalBox; i++){
+        squareGenerator();
+    };
+};
+
+function squareGenerator(){
+    const box = document.createElement('div');
+    box.classList = 'square';
+    grid.append(box);
+}
